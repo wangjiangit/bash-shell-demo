29e6c0381958fe5bfd8d6fa8651fc865fe88a5e2
wang jian <jiji_12342006@163.com>
2017-10-11 17:25:20 +0800

    gawk命令,统计nginx日志行数 普通PC上处理11.07G日志，处理时间1分55秒,将近3000万行

--- demo1.sh
+++ demo1.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash  -
 
-
-trap "echo Haha;echo asdf" EXIT
-echo 'this is a test program';
-
-count=1
-while  [ $count -le 2 ]
-do
-     echo "Loop #$count"
-     sleep 3
-      count=$[$count +1  ];
-done
-
-echo 'this is a end';
+#在命令行下运行一下命令,统计nginx日志行数
+#普通PC上处理11.07G日志，处理时间1分55秒,将近3000万行
+#gawk 'BEGIN{print strftime("%Y-%m-%d %H-%M-%S",systime());count=0}{++count }END{print strftime("%Y-%m-%d %H-%M-%S",sys
+#time());print count}' access1.log >state.log
