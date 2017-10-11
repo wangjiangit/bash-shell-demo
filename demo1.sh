7c81449e2ab64be4f22e14ee772ea2c4c76dc09e
wang jian <jiji_12342006@163.com>
2017-10-11 14:16:00 +0800

    添加 .gitignore文件

--- demo1.sh
+++ demo1.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash  -
 
-echo 'Hello Bash Shell';
+
+trap "echo Haha;echo asdf" EXIT
+echo 'this is a test program';
+
+count=1
+while  [ $count -le 2 ]
+do
+     echo "Loop #$count"
+     sleep 3
+      count=$[$count +1  ];
+done
+
+echo 'this is a end';
